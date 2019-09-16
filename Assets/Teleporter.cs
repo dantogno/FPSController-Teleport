d087858ea2b044003bf1273682ff3665854a8288
David Antognoli <david.antognoli@gmail.com>
2019-09-16 14:43:54 -0500

    committing teleportation example

---
+++ Assets/Teleporter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField]
+    private Transform destination;
+
+    private RigidbodyFirstPersonController playerController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // assuming there is only one of these in a scene!
+        playerController = FindObjectOfType<RigidbodyFirstPersonController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TeleportPlayerToDestination();
+        Debug.Log("Entered Trigger");
+    }
+
+    private void TeleportPlayerToDestination()
+    {
+        playerController.enabled = false;
+        playerController.transform.position = destination.position;
+        playerController.enabled = true;
+    }
+}
